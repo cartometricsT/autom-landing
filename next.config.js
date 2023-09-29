cbce3736b953aa51ebb2c7c643453f463e51eae6
pabloarca <146313878+cartometricsT@users.noreply.github.com>
2023-09-29 12:04:41 +0200

    next config

--- next.config.js
+++ next.config.js
@@ -1,5 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
-target: 'serverless',
-
-module.exports = nextConfig;
+const nextConfig = {
+    target: 'serverless', // Aquí se define la opción target
+    // Otras configuraciones pueden ir aquí
+  };
+  
+  module.exports = nextConfig;
